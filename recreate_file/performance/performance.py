bb5c880259d120a16a714e7acfbc1db9d8058612
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2020-11-23 09:31:00 -0500

    beefed up GUI, further streamlined recreate()

--- recreate_file/performance/performance.py
+++ recreate_file/performance/performance.py
@@ -1,29 +1,42 @@
-import time, os
+import time
+
+SAMPLE_SIZE = 100000
 
 class PerformanceCalc:
     def __init__(self, fileObj):
-        self.iter = []
-        self.cur_sum = 0
-        self.cur_count = 0 
-        print("Initial read speed test...")
-        for i in range(0, 100000):  # TODO change this to be dynamic in case the disk is small..?
-            start = time.perf_counter()
-            fileObj.read(512)  
-            stop = time.perf_counter()
-            self.iter.append(stop - start)
-        self.avg = (1000000 * sum(self.iter) / len(self.iter))
-        del self.iter[:]
-        print("Initial read speed test finished.")
-        fileObj.seek(0)
+        self.sample_size = SAMPLE_SIZE
+        self.avg = 0
 
-    def iteration(self, duration):
+        """ print("Initial read speed test...")
+        self.start()
+        while self.cur_incr < 1000:
+            tmp = fileObj.read(512)
+            print(tmp)
+            self.cur_incr += 1
+        cur_stop = time.perf_counter()
+        self.avg += (cur_stop - self.cur_start) * (self.sample_size / 1000)
+        print("Initial read speed test finished, initial average = " + "{:.2f}".format(self.avg))            """   
+
+
+    def start(self):
+        self.cur_incr = 0
+        self.cur_start = time.perf_counter()
+
+    def increment(self):
+        self.cur_incr += 1
+        if self.cur_incr >= self.sample_size:
+            cur_stop = time.perf_counter()
+            self.avg += (cur_stop - self.cur_start) 
+            self.avg = (self.avg / 2)
+            self.start()
+
+    """ def iteration(self, duration):
         self.cur_sum += duration
         self.cur_count += 1
-        if self.cur_count == 1000000:
-            cur_avg = 1000000 * self.cur_sum / self.cur_count        
+        if self.cur_count >= SECTOR_COUNT:
             #print("avg = " +  f"{self.avg:.3f}" + " + " + f"{cur_avg:.3f}")            
-            self.avg += cur_avg  
+            self.avg += self.cur_sum  
             self.avg = self.avg / 2  
             self.cur_sum = 0
             self.cur_count = 0
-            #print("avg = " +  f"{self.avg:.3f}" + " μs")
+            #print("avg = " +  f"{self.avg:.3f}" + " μs") """
--- recreate_file/recreate_file.py
+++ recreate_file/recreate_file.py
@@ -7,49 +7,39 @@ from PyQt5.QtWidgets import *
 from threading import Thread, Lock
 from concurrent import futures
 from multiprocessing import cpu_count
+from math import ceil
+from datetime import timedelta
 
-def check_sector(inp, addr, reader):
-    for b, found in reader.reference_file.sectors:
+
+def check_sector(inp, addr):
+#def check_sector():
+    for b in reader.reference_file.sectors:
         if inp == b:
-            if found == False:
-                i = reader.reference_file.sectors.index((b, found))
-                reader.rebuilt[i] = (addr, copy.deepcopy(b))
-                reader.successUpdate.emit(i)
-                reader.log.write(str(addr) + "\t\t" + str(i) + "\n")
-                reader.log.flush()
-                print("check_sector: sector at address " + str(addr) + " on disk is equal to sector " + str(i) + " of reference file.")
-                if i+1 == len(reader.reference_file.sectors):
-                    reader.finished = True
-            else:
-                print("Block " + str(i) + " was found at " + str(addr) + " but was already located at " + reader.rebuilt[i][1])
+            i = reader.reference_file.sectors.index(b)
+            reader.rebuilt[i] = (addr, copy.deepcopy(b))
+            reader.successUpdate.emit(i)
+            reader.log.write(str(addr) + "\t\t" + str(i) + "\n")
+            reader.log.flush()
+            print("check_sector: sector at logical address " + str(addr) + " on disk is equal to sector " + str(i) + " of reference file.")
+            if i+1 == len(reader.reference_file.sectors):
+                reader.finished = True
+            reader.reference_file.sectors.remove(b)
             return
     return
 
-def recreate(reader, start_at):
+def recreate(start_at):
 
-    executor = futures.ThreadPoolExecutor(max_workers=(cpu_count() / 2)) # TODO what is the correct number here? Surely it is not correct to take all available threads..?
-    lock = Lock()
-
-    #create required directories
-    if not os.path.exists('tmp'):
-        os.makedirs('tmp')
-    if not os.path.exists('results'):
-        os.makedirs('results')
+    executor = futures.ThreadPoolExecutor(max_workers=(cpu_count())) # TODO what is the correct number here? Surely it is not correct to take all available threads..?
 
     # start position in bytes
     reader.diskFd.seek(int(start_at, 16))
-
+    reader.perf.start()
     # main loop
     while not reader.finished:
-        with lock:  # TODO is lock necessary here?
-            start = time.perf_counter()
-            data = reader.diskFd.read(512)
-            stop = time.perf_counter()
-            reader.perf.iteration(stop - start)
-
-            executor.submit(check_sector, data, hex(reader.diskFd.tell() - 512), reader)
-
-            reader.progressUpdate.emit([reader.diskFd.tell(), reader.perf.avg])
+        executor.submit(check_sector, reader.diskFd.read(512), hex(reader.diskFd.tell() - 512))
+        reader.progressUpdate.emit([reader.diskFd.tell(), reader.perf.avg])        
+        reader.perf.increment()
+        #reader.diskFd.seek((len(reader.rebuilt) * 512), 1) # TODO 512 = ALLOCATION_UNIT
 
 class DiskReader(QtCore.QObject):    
     
@@ -68,7 +58,12 @@ class DiskReader(QtCore.QObject):
         self.reference_file = reference_file
         self.rebuilt = [None] * len(reference_file.sectors) 
         self.finished = False
-        self.log = open(self.reference_file.name + "_" + str(time.time()*1000) + ".log", 'w')
+
+        #create required directories
+        dir_name = 'ntfs-toolbox/' + 'recreate_file ' + time.ctime().replace(":", '_')
+        os.makedirs(dir_name, mode=0o755)
+
+        self.log = open(dir_name + '/' + self.reference_file.name + ".log", 'w')
         #self.main(start_at)
 
 
@@ -89,7 +84,7 @@ class ReferenceFile():
             if cur == b'':
                 break
             elif len(cur) == 512:
-                result.append((cur, False))
+                result.append(cur)
             else:
                 result.append((bytes.fromhex((cur.hex()[::-1].zfill(1024)[::-1])), False))   #trailing sector zfill
         return result
@@ -116,35 +111,45 @@ class MainWindow(QWidget):
         file_info_grid.addWidget(QLabel('Directory:'), 1, 0)
         file_info_grid.addWidget(QLabel(reference_file.dir), 1, 1)
         file_info_grid.addWidget(QLabel('Size:'), 2, 0)        
-        file_info_grid.addWidget(QLabel(str(reference_file.size / 512) + " sectors"), 2, 1)        
+        file_info_grid.addWidget(QLabel(str(ceil((reference_file.size / 512))) + " sectors"), 2, 1)        
 
         start_at_hbox = QHBoxLayout()
         start_at_label = QLabel("Start at address (search forward): ")
         self.start_at = QLineEdit()
-        self.start_at.setText("0x6AAC73400")
+        self.start_at.setText('0x404A8A99000')
+        #self.start_at.setText('0x404C7A99000')
         start_at_hbox.addWidget(start_at_label)
         start_at_hbox.addWidget(self.start_at)
 
         successes_hbox = QHBoxLayout()        
         self.successes = QLabel()
-        self.successes.setText("0/0")
+        self.successes.setText("0/" + (str(ceil((reference_file.size / 512)))))
         successes_hbox.addWidget(self.successes)
         self.progressBar = QProgressBar()
         self.progressBar.setTextVisible(False)
         self.start = QPushButton('Start')
         self.progressPercentage = QLabel()
-        self.sectorAverage = QLabel()
-        
-        self.reader = DiskReader(selected_vol, reference_file)
-        
+        self.sector_avg = QLabel()
+        self.time_remaining = QLabel()
+
+        global reader
+        reader = DiskReader(selected_vol, reference_file)
         self.start.clicked.connect(self.go)
 
         grid = QGridLayout()        
         grid.addLayout(file_info_grid, 0, 0)
-        grid.addWidget(self.progressPercentage, 0, 2)
-        grid.addWidget(self.sectorAverage, 1, 2)
+
+        self.progressPercentage.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        grid.addWidget(self.progressPercentage, 7, 2)
+        
+        self.sector_avg.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        grid.addWidget(self.sector_avg, 9, 2)
+
+        self.time_remaining.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)         
+        grid.addWidget(self.time_remaining, 8, 2)
+
         grid.addLayout(successes_hbox, 3, 0)        
-        grid.addWidget(self.progressBar, 4, 0, 4, 3)        
+        grid.addWidget(self.progressBar, 4, 0, 4, 3)       
         grid.addWidget(self.start, 9, 0)
         grid.addLayout(start_at_hbox, 10, 0) 
 
@@ -156,28 +161,36 @@ class MainWindow(QWidget):
         if re.search('^0x\d+', start_at_input) == None:
             print('not correct pattern')
             return
-        self.reader.progressUpdate.connect(self.updateProgress)
-        self.reader.successUpdate.connect(self.updateSuccessCount)
+        reader.progressUpdate.connect(self.updateProgress)
+        reader.successUpdate.connect(self.updateSuccessCount)
      
         self.start.setText('...')
         self.start.setDisabled(True)
         self.start_at.setDisabled(True)  
-        recreate_main = Thread(name='recreate main',target=recreate,args=[self.reader, self.start_at.text().split("x")[1]])
+        recreate_main = Thread(name='recreate main',target=recreate,args=[self.start_at.text().split("x")[1]])
         recreate_main.start() 
         # ...
         #recreate_main.join()
         #print("Program terminated")
-        
 
     def updateSuccessCount(self, i):
         successCount = 0 
-        for entry in self.reader.rebuilt:
+        for entry in reader.rebuilt:
             if entry != None:
                 successCount += 1
         
-        self.successes.setText("sector " + str(i) + " rebuilt: " + str(successCount) + "/" + str(len(self.reader.rebuilt)))
+        s = ("Last match: sector " + str(i) + "\n\n")
+        s += (str(successCount) + "/" + str(len(reader.rebuilt)) + " = " + "{:.4f}".format(successCount / len(reader.rebuilt)) + "%\n\n")
+        s += ("Checking equality for " + str(len(reader.reference_file.sectors)) + " remaining sectors...")
+        self.successes.setText(s)
         
     def updateProgress(self, progress):
-        self.progressPercentage.setText("{:.7f}".format(100 * progress[0] / self.reader.diskSize.total) + "%")
-        self.sectorAverage.setText("Average time per sector read: " + "{:.2f}".format(progress[1]) + " μs")
-        self.progressBar.setValue(100 * progress[0] / self.reader.diskSize.total)     
+        percent = 100 * progress[0] / reader.diskSize.total
+        self.progressPercentage.setText("{:.7f}".format(percent) + "%")
+        self.sector_avg.setText("Average time to read and parse " + str(reader.perf.sample_size) + " sectors (" + str(reader.perf.sample_size * 512 / 1000000) + " MB): {:.2f}".format(progress[1]) + " seconds")
+        self.progressBar.setValue(percent)     
+        self.time_remaining.setText(self.get_remaining_estimate(progress))
+
+    def get_remaining_estimate(self, progress):
+        seconds = reader.perf.avg * ((reader.diskSize.total - progress[0]) / (512 * reader.perf.sample_size))
+        return str(timedelta(seconds=seconds)).split(".")[0] + " remaining"
