9d7ffc19cdceed21584e0a973e7fca16467ccffb
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2020-12-19 11:53:35 -0500

    improve initial dialog for removal of raw_gz

--- init.py
+++ init.py
@@ -1,70 +1,68 @@
 import string
 import os
-from PyQt5.QtWidgets import QDialog, QHBoxLayout, QLabel, QComboBox, QDialogButtonBox, QVBoxLayout, QApplication
+from PyQt5.QtWidgets import QFileDialog, QDialog, QHBoxLayout, QLabel, QComboBox, QDialogButtonBox, QVBoxLayout, QApplication, QCheckBox
 from recreate_file import gui
+
+class ChooseSourceFileDialog(QFileDialog):
+    def __init__(self):
+        super(ChooseSourceFileDialog, self).__init__()
+        self.setWindowTitle("Choose source file")
 
 class StartDialog(QDialog):
     def __init__(self):
 
         super(StartDialog, self).__init__()
-        self.setWindowTitle("raw-gz")
 
-        vols = ['%s:' % v for v in string.ascii_uppercase if os.path.exists('%s:' % v)]
-        progs = ["locate and unpack .gzip archives", "recreate file"]
-
-        hbox1 = QHBoxLayout()
-        vol_select_label = QLabel("Logical volume: ")
+        vbox = QVBoxLayout()
+        label = QLabel("Select the corrupt disk or logical volume of interest.")
         self.vol_select_dropdown = QComboBox(self)
+        vbox.addWidget(label)
+        vbox.addWidget(self.vol_select_dropdown)
 
-        self.vol_select_dropdown.addItems(vols)
+        self.include_raw = QCheckBox("Include raw disks ")
+        self.include_raw.setStyleSheet("font-style:italic")
+        self.include_raw.stateChanged.connect(self.render_vols)
+        self.include_raw.setChecked(True)
+
         self.vol_select_dropdown.setCurrentText("D:")    # TODO remove
-        hbox1.addWidget(vol_select_label)
-        hbox1.addWidget(self.vol_select_dropdown)
-
-        hbox2 = QHBoxLayout()
-        prog_select_label = QLabel("Program: ")
-        self.prog_select_dropdown = QComboBox(self)
-        self.prog_select_dropdown.addItems(progs)
-        self.prog_select_dropdown.setCurrentText("recreate file")  # TODO remove
-
-        hbox2.addWidget(prog_select_label)
-        hbox2.addWidget(self.prog_select_dropdown)
-
         qbtn = QDialogButtonBox.Ok
         self.button_box = QDialogButtonBox(qbtn)
         self.button_box.accepted.connect(self.accept)
+        vbox.addWidget(self.include_raw)
+        vbox.setSpacing(10)
 
+        layout = QVBoxLayout()
+        layout.addLayout(vbox)        
+        layout.addWidget(self.button_box)
+        layout.setContentsMargins(50, 50, 50, 50)
+        layout.setSpacing(50)
+        self.setLayout(layout)
 
-        vert = QVBoxLayout()
-        vert.addLayout(hbox1)
-        vert.addLayout(hbox2)
-        vert.addWidget(self.button_box)
-        self.setLayout(vert)
+    def render_vols(self):
+        if self.include_raw.isChecked():
+            vols = ['%s:' % v for v in string.ascii_uppercase if os.path.exists('%s:' % v)]
+            vols += ['Raw disk %s' % d for d in range(50) if os.path.exists('\\\\.\\PhysicalDrive%s' % d)]
+        else:
+            vols = ['%s:' % v for v in string.ascii_uppercase if os.path.exists('%s:' % v)]
 
+        self.vol_select_dropdown.clear()
+        self.vol_select_dropdown.addItems(vols)
 
 app = QApplication([])
 
-dlg = StartDialog()
-dlg.setWindowTitle('ntfs-parse')
+disk_select = StartDialog()
+disk_select.setWindowTitle('recoverability')
+disk_select.exec()
 
-dlg.show()
+selected_vol = disk_select.vol_select_dropdown.currentText()[0]
+
+file_select = ChooseSourceFileDialog()
+file_select.exec()
+path = file_select.selectedFiles()[0]
+
+window = gui.MainWindow(selected_vol, path)
+window.setWindowTitle('recoverability')
+window.setGeometry(500, 500, 500, 600)
+window.show()
+
 app.exec_()
-
-selected_vol = dlg.vol_select_dropdown.currentText()[0]
-selected_prog = dlg.prog_select_dropdown.currentText()
-
-if selected_prog == "locate and unpack .gzip archives":
-    new_app = QApplication([])
-    window = raw_gz.MainWindow(selected_vol)
-    window.setWindowTitle('raw_gz')
-    window.setGeometry(500, 500, 500, 300)
-    window.show()
-    new_app.exec_()
-
-elif selected_prog == "recreate file":
-    new_app = QApplication([])
-    window = gui.MainWindow(selected_vol)
-    window.setWindowTitle('recreate_file')
-    window.setGeometry(500, 500, 500, 600)
-    window.show()
-    new_app.exec_()
